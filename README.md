03ccd95aa6f93d478e17cc4506434f13bf780bdd
zoey <zuyicai@umich.edu>
2019-03-18 18:38:36 -0400

    modified readme and edited about routes and models in main file

---
+++ README.md
@@ -0,0 +1,119 @@
+# Project3_zuyicai: A Simple Movie Flask App
+
+This's a whole web application — a very simple one, using the web framework called Flask. Using this small application, you can get different outputs in the form of URLs. In this project, I created the database and added information to this database by using different URLs and different input.
+
+SI507_project3.py is the file that I create the class and define the functional structure of this system, plus the routes; templates is the file that I create the htmls for different appearance of URLs. Here I referred to the diagram of the database that I created for project2. In this database, "Rating" to "movie" is one to one, "Director" to "movie" is many to one, "Distributor" to "movie" is many to one, "Distributor" to "Director" is many to many(Here I just show the relationship).
+
+* ![Alt text](https://github.com/zuyicai/image/blob/master/project3/SI507_movies_database_plan.key)
+
+
+Actually, I improve the app by adding serval routes such as all_distributors, all_directors and ratings, and this app has routes to add details to director and distributor. Thus readers can get the whole picture of the movie with the director, the distributor and different ratings.
+
+Readers need to follow the steps to install everything needed to run this application.
+
+## Getting Started
+
+* Anaconda installed
+* Open your terminal window! `cd` to the place where you want this project to go.
+* This repository cloned to somewhere in your computer (the place).
+```
+git clone <git url>
+```
+* `cd` into where the project lives
+* Create a virtual environment for it
+```
+virtualenv env
+```
+* Activate the virtual environment
+```
+$ source <projectname>-env/bin/activate    # For Mac/Linux...
+$ source <projectname>-env/Scripts/activate    # For Windows
+(project3-env) $     # you've succeeded if you see this after!
+```
+* install all requirement
+```
+pip install -r requirements.txt
+```
+```
+Deactivate
+```
+* Just run the movie database app!
+```
+python SI507_project3.py runserver
+```
+* Check out what’s happening in your terminal window!
+* Open a web browser, type in and check out this URL:
+http://127.0.0.1:5000/
+* you will see "number movies recorded". Here number will depend upon how many movies are already in your database.
+* ![Alt text](https://github.com/zuyicai/image/blob/master/project3/home.png)
+
+* Open another tab and go to:
+http://127.0.0.1:5000/Movie/new/moonriver/Jack/DreamCo/8.0(This is an example of a movie called moonriver with Rotten_Tomatoes rating 8.0, which is directed by Jack and distributed by DreamCo)
+* After this you will see the following image. (Here the name of movie will depend on what you input in the URL:<title>, the director of movie will depend on what you input in the URL:<director>, the distributor of movie will depend on what you input in the URL:<distributor>.)
+* ![Alt text](https://github.com/zuyicai/image/blob/master/project3/movie1.png)
+* If we already have this movie in our database, it will shows the following information:
+* ![Alt text](https://github.com/zuyicai/image/blob/master/project3/movie2.png)
+* This route is wrote with the use of "render_template". It's what we learned recently in class. And this one has to do with the html file in templates file.
+
+* Now try going to:
+http://127.0.0.1:5000/all_movies
+* This route shows the list of all movies saved with the distributor and director.
+* you will see the following image.
+* ![Alt text](https://github.com/zuyicai/image/blob/master/project3/allm.png)
+
+
+* Then:
+http://127.0.0.1:5000/all_directors
+* This route shows the list of all directors and the number of movies they directed.
+* you will see the following image.
+* ![Alt text](https://github.com/zuyicai/image/blob/master/project3/alldir.pngg)
+
+
+* Then:
+http://127.0.0.1:5000/all_distributors
+* This route shows the list of all distributors and the number of movies they distributed.
+* you will see the following image.
+* ![Alt text](https://github.com/zuyicai/image/blob/master/project3/alldis.png)
+
+* Then:
+http://127.0.0.1:5000/moonriver/7/8/9/7.7
+* This route is the way we add ratings information to existed movies.
+* you will see the following image.
+* ![Alt text](https://github.com/zuyicai/image/blob/master/project3/rating.png)
+* If the movie you input doesn't exist, then you will see the following image.
+* ![Alt text](https://github.com/zuyicai/image/blob/master/project3/rating2.png)
+
+* Then:
+http://127.0.0.1:5000/Director/Jack/1972.10.09/theUS/USparty
+* This route is the way we add director's information to existed directors.
+* you will see the following image.
+* ![Alt text](https://github.com/zuyicai/image/blob/master/project3/dir.png)
+* If the director you input doesn't exist, then you will see the following image.
+* ![Alt text](https://github.com/zuyicai/image/blob/master/project3/dir2.png)
+
+* Then:
+http://127.0.0.1:5000/Distributor/DreamCo/Los%20Angeles
+* This route is the way we add distributor's information to existed distributors.
+* you will see the following image.
+* ![Alt text](https://github.com/zuyicai/image/blob/master/project3/dis.png)
+* If the distributor you input doesn't exist, then you will see the following image.
+* ![Alt text](https://github.com/zuyicai/image/blob/master/project3/dis2.png)
+
+
+
+## After all, you will get a database with the information you input in.
+
+* Here I show a example which is what I created.
+* The database structure:
+* ![Alt text](https://github.com/zuyicai/image/blob/master/project3/structure.png)
+* The Ratings and details:
+* ![Alt text](https://github.com/zuyicai/image/blob/master/project3/ratings.png)
+* The movies and details:
+* ![Alt text](https://github.com/zuyicai/image/blob/master/project3/movies.png)
+* The directors and details:
+* ![Alt text](https://github.com/zuyicai/image/blob/master/project3/directors.png)
+* The distributors and details:
+* ![Alt text](https://github.com/zuyicai/image/blob/master/project3/distributors.png)
+
+
+* Exit it / stop it running on the local server by typing `Control + C`
--- SI507_project3.py
+++ SI507_project3.py
@@ -22,7 +22,7 @@ session = db.session # to make queries easy
 
 ### Models
 
-# Set up association Table between artists and albums
+# Set up association Table between distributors and directors: many to many. In this database, I didn't write this relationship.
 collections = db.Table('collections',
     db.Column('Director_id', db.Integer, db.ForeignKey('director.id'), primary_key=True),
     db.Column('Distributor_id', db.Integer, db.ForeignKey('distributor.id'), primary_key=True))
@@ -35,7 +35,6 @@ class Distributor(db.Model):
     # movie_id = db.Column(db.Integer, db.ForeignKey("movie.id"))
     location = db.Column(db.String(64))
     movies = db.relationship("movie",backref="Distributor") # Distributor-movie:one to many
-    # directors = db.relationship('Director',secondary=collections,backref=db.backref('distributor',lazy='dynamic'),lazy='dynamic')
     directors = db.relationship('Director', secondary=collections, lazy='subquery', backref=db.backref('Distributor', lazy=True))
 
 
@@ -47,14 +46,13 @@ class Director(db.Model):
     __tablename__ = "director"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
-    # movie_id = db.Column(db.Integer, db.ForeignKey("movie.id"))
     Birth = db.Column(db.String(64))
     Nationality = db.Column(db.String(64))
     Party = db.Column(db.String(64))
     movies = db.relationship("movie",backref="Director") # Director-movie:one to many
 
     def __repr__(self):
-        return "{} by {} | {}".format(self.movie,self.name, self.Nationality)
+        return "{}'s personal information: Birth: {}, Nationality:{}, Party:{}.".format(self.name,self.Birth, self.Nationality,self.Party)
 
 
 class Rating(db.Model):
@@ -66,6 +64,9 @@ class Rating(db.Model):
     Rotten_Tomatoes = db.Column(db.REAL)
     IMDB_Rating = db.Column(db.REAL)
     IMDB_Votes = db.Column(db.REAL)
+
+    def __repr__(self):
+        return"{} has ratings: MPAA_Rating {}, Rotten_Tomatoes {}, IMDB_Rating {}, IMDB_Votes {}.".format(self.moname,self.MPAA_Rating,self.Rotten_Tomatoes,self.IMDB_Rating,self.IMDB_Votes)
 
 
 class movie(db.Model):
@@ -81,7 +82,7 @@ class movie(db.Model):
 
 
     def __repr__(self):
-        return "{} by {} | {}".format(self.title,self.Director, self.Distributor)
+        return "{} by director {} |  Distributor {}. {} has ratings information, please refer to {}".format(self.title,self.Director_id, self.Distributor_id, self.title, self.rating_id)
 
 
 
@@ -91,6 +92,8 @@ class movie(db.Model):
 ### For database additions
 ### Relying on global session variable above existing
 
+
+# get or create director
 def get_or_create_director(director_name):
     director = Director.query.filter_by(name=director_name).first()
     if director:
@@ -101,6 +104,7 @@ def get_or_create_director(director_name):
         session.commit()
         return director
 
+# get or create distributor
 def get_or_create_distibutor(distributor_name):
     distributor = Distributor.query.filter_by(name=distributor_name).first()
     if distributor:
@@ -111,16 +115,8 @@ def get_or_create_distibutor(distributor_name):
         session.commit()
         return distributor
 
-# def get_or_create_rating(movie_title,rating_MPAA,rating_Rotten,rating_IMDB_R,rating_IMDB_V):
-#     rating = Rating.query.filter_by(moname=movie_title, MPAA_Rating=rating_MPAA, Rotten_Tomatoes=rating_Rotten, IMDB_Rating=rating_IMDB_R, IMDB_Votes=rating_IMDB_V).first()
-#     if rating:
-#         return rating
-#     else:
-#         rating = Rating(moname=movie_title, MPAA_Rating=rating_MPAA, Rotten_Tomatoes=rating_Rotten, IMDB_Rating=rating_IMDB_R, IMDB_Votes=rating_IMDB_V)
-#         session.add(rating)
-#         session.commit()
-#         return rating
-
+# get or create rating, here I only create Rotten_Tomatoes rating because it's much typical and we don't need to list all ratings when creating the movieself.
+# We will have detailed ratings information by special route to create and add ratings to certain movie.
 def get_or_create_rating(movie_title,rating_Rotten):
     rating = Rating.query.filter_by(moname = movie_title).first()
     if rating:
@@ -138,6 +134,7 @@ def get_or_create_rating(movie_title,rating_Rotten):
 #### Set up Controllers (route functions) #####
 
 ## Main route
+# This route is a basic route to welcome readers~ And it shows the number of movies already existed in this database.
 @app.route('/') # http://127.0.0.1:5000/
 def index():
     movies = movie.query.all()
@@ -146,18 +143,17 @@ def index():
 
 @app.route('/Movie/new/<title>/<director>/<distributor>/<rating>') # http://127.0.0.1:5000/Movie/new/moonriver/Jack/DreamCo/8.0
 def new_movie(title, director, distributor, rating):
-    if movie.query.filter_by(title=title).first(): # if there is a song by that title
-        return "That movie already exists! Go back to the main app!"
-    else:
-        # distributor = Distributor(name=distributor_name)
-        director = get_or_create_director(director)
-        distributor = get_or_create_distibutor(distributor)
-        rating = get_or_create_rating(title,rating)
-        newmo = movie(title=title, Director_id = director.id, Distributor_id = distributor.id, rating_id = rating.id)
-        session.add(newmo)
-        session.commit()
-        return "New movie: {} by {}. Check out the URL for ALL movies to see the whole list.".format(newmo.title, director.name)
-        # return "The movie doesn't have rating info"
+    if movie.query.filter_by(title=title).first():# if there is a movie by that title
+       return "That movie already exists! Go back to the home page!"
+    else: # If not, we will create this movie and add it to our database
+       director = get_or_create_director(director)
+       distributor = get_or_create_distibutor(distributor)
+       rating = get_or_create_rating(title,rating)
+       newmo = movie(title=title, Director_id = director.id, Distributor_id = distributor.id, rating_id = rating.id)
+       session.add(newmo)
+       session.commit()
+       return render_template('new_movie.html',new_movie = newmo.title,new_director = newmo.Director_id,new_distributor = newmo.Distributor_id,new_rating = newmo.rating_id)
+        # return "New movie: {} by {}. Check out the URL for ALL movies to see the whole list.".format(newmo.title, director.name)
 
 
 @app.route('/all_movies') # http://127.0.0.1:5000/all_movies
@@ -170,7 +166,7 @@ def see_all():
         all_movies.append((mo.title,director.name, distributor_name.name)) # get list of songs with info to easily access [not the only way to do this]
     return render_template('all_movies.html',all_movies=all_movies) # check out template to see what it's doing with what we're sending!
 
-@app.route('/all_directors')
+@app.route('/all_directors') # http://127.0.0.1:5000/all_directors
 def see_all_directors():
     directors = Director.query.all()
     names = []
@@ -180,7 +176,7 @@ def see_all_directors():
         names.append(newtup) # names will be a list of tuples
     return render_template('all_directors.html',director_names=names)
 
-@app.route('/all_distributors')
+@app.route('/all_distributors') #http://127.0.0.1:5000/all_distributors
 def see_all_distributor():
     distributors = Distributor.query.all()
     names = []
@@ -191,10 +187,10 @@ def see_all_distributor():
     return render_template('all_distributors.html',distributor_names=names)
 
 # This route is for inputting ratings info.
-@app.route('/<movie_title>/<rating_MPAA>/<rating_Rotten>/<rating_IMDB_R>/<rating_IMDB_V>')
+@app.route('/<movie_title>/<rating_MPAA>/<rating_Rotten>/<rating_IMDB_R>/<rating_IMDB_V>') # http://127.0.0.1:5000/moonriver/7/8/9/7.7
 def see_all_ratings(movie_title, rating_MPAA, rating_Rotten, rating_IMDB_R, rating_IMDB_V):
     if movie.query.filter_by(title = movie_title).first():
-        themovie = Rating.query.filter_by(Rotten_Tomatoes = rating_Rotten).first()
+        themovie = Rating.query.filter_by(moname = movie_title, Rotten_Tomatoes = rating_Rotten).first()
         themovie.MPAA_Rating = rating_MPAA
         themovie.Rotten_Tomatoes = rating_Rotten
         themovie.IMDB_Rating = rating_IMDB_R
@@ -204,10 +200,10 @@ def see_all_ratings(movie_title, rating_MPAA, rating_Rotten, rating_IMDB_R, rati
         session.commit()
         return "That movie {} has ratings! MPAA_Rating:{}. Rotten_Tomatoes:{}. IMDB_Rating:{}. IMDB_Votes:{}.".format(movie_title, themovie.MPAA_Rating,themovie.Rotten_Tomatoes,themovie.IMDB_Rating,themovie.IMDB_Votes)
     else:
-        return "That movie doesn't have record. Please input the info to check"
+        return "That movie doesn't have record. Please input the information and then to check."
 
 # This route is for inputting directors info.
-@app.route('/Director/<director>/<birth>/<nationality>/<party>')
+@app.route('/Director/<director>/<birth>/<nationality>/<party>') # http://127.0.0.1:5000/Director/Jack/1972.10.09/theUS/USparty
 def see_directors(director,birth,nationality,party):
     if Director.query.filter_by(name = director).first():
         thedir = Director.query.filter_by(name = director).first()
@@ -223,11 +219,11 @@ def see_directors(director,birth,nationality,party):
         names.append(newtup)
         return render_template('all_directors.html',director_names=names)
     else:
-        return "That director doesn't have record. Please input the info to check"
+        return "That director doesn't have record. Please input the information and then to check."
 
 
 # This route is for inputting distributors info.
-@app.route('/Distributor/<distributor>/<location>')
+@app.route('/Distributor/<distributor>/<location>') # http://127.0.0.1:5000/Distributor/DreamCo/Los%20Angeles
 def see_distributors(distributor,location):
     if Distributor.query.filter_by(name = distributor).first():
         thedis = Distributor.query.filter_by(name = distributor).first()
@@ -241,7 +237,7 @@ def see_distributors(distributor,location):
         names.append(newtup)
         return render_template('all_distributors.html',distributor_names=names)
     else:
-        return "That distributor doesn't have record. Please input the info to check"
+        return "That distributor doesn't have record. Please input the information and then to check."
 
 
 
